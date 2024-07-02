e63d0b0569613d3dd699ea6a25d88481767c784e
umarahh4u <umarahh4u@gmail.com>
2024-07-02 23:38:34 +0100

    update core

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const middlewares = require("./middleware");
 const app = express();
 
 // body parser
+app.use(cors());
 app.use(express.json({ limit: "10kb" }));
 app.use(express.urlencoded({ extended: true, limit: "10kb" }));
 
